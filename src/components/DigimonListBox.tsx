58cadfaf11766f34b0e40a43756b4da13ae282cf
Ivan Henriquez <ivan.henriquez@omegaccs.com>
2023-07-28 11:14:59 -0400

    probando touchemove mobile

--- src/components/DigimonListBox.tsx
+++ src/components/DigimonListBox.tsx
@@ -33,9 +33,11 @@ const DigimonListBox = ({ data }: any) => {
     setIsLoading(false);
 
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("touchmove", handleScroll);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("touchmove", handleScroll);
     };
   }, [page]);
 
